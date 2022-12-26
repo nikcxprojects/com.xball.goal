045d41fa21ac6a8d67d5c69774ca1dc5c06a2233
Sladkiy Eduard <nikcxprojects@gmail.com>
2022-12-26 13:52:28 +0300

    updaate

--- Assets/com.bestball.three.game/Scripts/Country.cs
+++ Assets/com.bestball.three.game/Scripts/Country.cs
@@ -4,11 +4,13 @@ using UnityEngine.UI;
 public class Country : MonoBehaviour
 {
     [SerializeField] string _name;
+    [SerializeField] Sprite sprite;
+
     private void Start()
     {
         GetComponent<Button>().onClick.AddListener(() =>
         {
-            UIManager.Instance.SetCountry(GetComponent<Image>().sprite, _name);
+            UIManager.Instance.SetCountry(sprite, _name);
         });
     }
 }
